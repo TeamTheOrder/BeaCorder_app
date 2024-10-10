64345e66f70e04d2b98bf440abf4d3873f2f6eda
BlooMir <binarywaterdragon@gmail.com>
2024-10-10 17:10:22 +0900

    fix : blue_plus 라이브러리 삭제

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -6,16 +6,9 @@
 
 #import "GeneratedPluginRegistrant.h"
 
-#if __has_include(<flutter_blue_plus/FlutterBluePlusPlugin.h>)
-#import <flutter_blue_plus/FlutterBluePlusPlugin.h>
-#else
-@import flutter_blue_plus;
-#endif
-
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [FlutterBluePlusPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterBluePlusPlugin"]];
 }
 
 @end
